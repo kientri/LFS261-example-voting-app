835aa2471e054458e5f1d4a941defbd6e7c51ad8
Your Name <you@example.com>
2023-02-15 06:26:07 +0000

    cat

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "DOG")
+option_b = os.getenv('OPTION_B', "CAT")
 hostname = socket.gethostname()
 version = 'v1'
 
